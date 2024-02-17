f58796eb3183927f879b554e0fade1182b309466
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-02-17 17:20:52 +0100

    test is green, which checks if a dummpy json response can be fetched from a test HTTP-server

--- src/main/java/de/expeditionengineer/app/CurrencyConverter.java
+++ src/main/java/de/expeditionengineer/app/CurrencyConverter.java
@@ -2,12 +2,19 @@ package de.expeditionengineer.app;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Properties;
+
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.URI;
+
 
 /**
  * Hello world!
@@ -30,8 +37,14 @@ public class CurrencyConverter
         }
     }
     
-    public void loadExchangeData() {
-        String apiURL = "http://127.0.0.1:8000";
+    public String loadExchangeData(Boolean test) {
+        String apiURL = "";
+        if (test) {
+            apiURL = "http://127.0.0.1:8000";
+        }
+        else {
+            apiURL = "https://v6.exchangerate-api.com/v6/" + exchangeAPIKey + "/latest/EUR";
+        }
         System.out.println("Doing a fetch request to the URL: " + apiURL);
 
         HttpClient client = HttpClient.newHttpClient();
@@ -40,8 +53,14 @@ public class CurrencyConverter
                 .build();
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(response.body());
+            JsonObject jsonobj = root.getAsJsonObject();
+            return jsonobj.get("result").getAsString();
+
         } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("An error occured while fetching the exchange data. Maybe the API-Key is invalid?");
+            return "error";
         }
     }
 
--- src/test/java/de/expeditionengineer/app/AppTest.java
+++ src/test/java/de/expeditionengineer/app/AppTest.java
@@ -35,8 +35,9 @@ public class AppTest
             System.out.println("Coudlnt start the HTTP-Server. Maybe port 8000 is already in use?");
         }
         CurrencyConverter converterObj = new CurrencyConverter();
-        converterObj.loadExchangeData();
+        String reportIfDataFetchWasSuccessful = converterObj.loadExchangeData(true);
         assertEquals(1, handler.getCount());
+        assertTrue(reportIfDataFetchWasSuccessful.equals("success"));
     }
     static class MyHandler implements HttpHandler {
         private int count = 0; // counter variable
@@ -46,7 +47,27 @@ public class AppTest
             count++; // increment the counter each time handle is called
             System.out.println("Handle method called " + count + " times");
     
-            String response = "This is the response";
+            String response = "{\n" + //
+                                "\t\"result\": \"success\",\n" + //
+                                "\t\"documentation\": \"https://www.exchangerate-api.com/docs\",\n" + //
+                                "\t\"terms_of_use\": \"https://www.exchangerate-api.com/terms\",\n" + //
+                                "\t\"time_last_update_unix\": 1585267200,\n" + //
+                                "\t\"time_last_update_utc\": \"Fri, 27 Mar 2020 00:00:00 +0000\",\n" + //
+                                "\t\"time_next_update_unix\": 1585353700,\n" + //
+                                "\t\"time_next_update_utc\": \"Sat, 28 Mar 2020 00:00:00 +0000\",\n" + //
+                                "\t\"base_code\": \"USD\",\n" + //
+                                "\t\"conversion_rates\": {\n" + //
+                                "\t\t\"USD\": 1,\n" + //
+                                "\t\t\"AUD\": 1.4817,\n" + //
+                                "\t\t\"BGN\": 1.7741,\n" + //
+                                "\t\t\"CAD\": 1.3168,\n" + //
+                                "\t\t\"CHF\": 0.9774,\n" + //
+                                "\t\t\"CNY\": 6.9454,\n" + //
+                                "\t\t\"EGP\": 15.7361,\n" + //
+                                "\t\t\"EUR\": 0.9013,\n" + //
+                                "\t\t\"GBP\": 0.7679\n" + //
+                                "\t}\n" + //
+                                "}";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
